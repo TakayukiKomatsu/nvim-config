50e5a6048b85256c225032f9f41e0c16df2c77f6
Takayuki Komatsu <komatsutakayuki235@live.com>
2024-08-25 19:06:47 -0300

    chore: add flash

--- lua/plugins/substitute.lua
+++ lua/plugins/substitute.lua
@@ -9,9 +9,9 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
-    keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
-    keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
-    keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
+    keymap.set("n", "<leader>ms", substitute.operator, { desc = "Substitute with motion" })
+    keymap.set("n", "<leader>ml", substitute.line, { desc = "Substitute line" })
+    keymap.set("n", "<leader>mS", substitute.eol, { desc = "Substitute to end of line" })
+    keymap.set("x", "<leader>ms", substitute.visual, { desc = "Substitute in visual mode" })
   end,
 }
