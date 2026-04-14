b1a8cbfb7e49253c2acf9d8a55730f5f596fcee8
Takayuki Komatsu <komatsutakayuki235@live.com>
2026-04-14 14:45:47 -0300

    docs(plugins): clarify intentional organization exceptions

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
--- Disable LazyVim default plugins we don't use
+-- Intentional exception file:
+-- grouped upstream-plugin disables live here so maintainers can find opt-out decisions in one place.
+-- Keep this file focused on explicit disable/replace decisions only.
 return {
   { "nvim-neo-tree/neo-tree.nvim",  enabled = false }, -- using nvim-tree instead
   { "folke/tokyonight.nvim",        enabled = false }, -- using witch instead
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,6 @@
+-- Intentional exception file:
+-- shared foundational plugins that do not justify their own standalone spec files yet.
+-- Keep this file small and limited to truly cross-cutting dependencies.
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
